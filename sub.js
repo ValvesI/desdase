ddb26367c56b129e4474537f269ffef747d40d7a
ValvesI <131627287+ValvesI@users.noreply.github.com>
2024-08-16 12:14:18 -0300

    .

---
+++ sub.js
@@ -0,0 +1,68 @@
+let atual = 0;
+let perguntaAtual;
+
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPergunta.textContent = perguntaAtual.enunciado; 
+}
+
+mostraAlternativa(){
+    for (const pergunta of perguntaAtual.alternativas){
+        const botaoAlternativa = document.createElement('button');
+        botaoAlternativa.textContent = pergunta;
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+}
+
+
+
+
+
+const perguntas = [
+{
+    enunciado: "ambito 1",
+    alternativas: [
+        {
+            texto: "alternativa 1",
+            afirmação: "afirmação",
+        },
+        {
+            texto: "alternativa 2",
+            afirmação: "afirmação",
+        },
+        
+    ]
+},
+{
+    enunciado: "ambito 2",
+    alternativas: [
+        {
+            texto: "alternativa 1",
+            afirmação: "afirmação",
+        },
+        {
+            texto: "alternativa 2",
+            afirmação: "afirmação",
+        },
+        
+    ]
+},
+{
+    enunciado: "ambito 3",
+    alternativas: [
+        {
+            texto: "alternativa 1",
+            afirmação: "afirmação",
+        },
+        {
+            texto: "alternativa 2",
+            afirmação: "afirmação",
+        },
+        
+    ]
+},
+]
+
+mostraAlternativa();
+mostraPergunta();
